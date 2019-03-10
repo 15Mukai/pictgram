1090ecf68643c07a1708d338414ba94ccdf2b828
mukaiyumu <yumumukai17@gmail.com>
2019-03-10 14:58:54 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'pages/index'
+  root 'pages#index'
+end
+Rails.application.routes.draw do
+  get 'users/new'
+  root 'pages#index'
+  get 'pages/help'
+end
+Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/help'
+
+  resources :users
+end
