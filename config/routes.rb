5f3993b58f207d238c6aefdeb3bc40d4e4e27d5d
mukaiyumu <yumumukai17@gmail.com>
2019-03-14 02:22:24 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,31 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get 'pages/index'
   root 'pages#index'
 end
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'pages#index'
   get 'pages/help'
 end
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'pages#index'
   get 'pages/help'
 
   resources :users
+end
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'pages#index'
+  get 'pages/help'
+
+  resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
