106ae9319496b502892d435dcf7edbbe7c4e8098
mukaiyumu <yumumukai17@gmail.com>
2019-03-15 17:14:12 +0900

    third commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :info, :warning, :danger
 
+  helper_method :current_user, :logged_in?
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
@@ -10,5 +12,3 @@ class ApplicationController < ActionController::Base
     !current_user.nil?
   end
 end
-
-helper_method :current_user
