7de74c9244efc4dcddab890a613f07a32b4b0481
mukaiyumu <yumumukai17@gmail.com>
2019-03-21 20:51:03 +0900

    routesの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,4 @@
 Rails.application.routes.draw do
-  get 'topics/new'
-  get 'sessions/new'
-  get 'users/new'
-  get 'pages/index'
-  root 'pages#index'
-end
-Rails.application.routes.draw do
-  get 'topics/new'
-  get 'sessions/new'
-  get 'users/new'
-  root 'pages#index'
-  get 'pages/help'
-end
-Rails.application.routes.draw do
-  get 'topics/new'
-  get 'sessions/new'
-  root 'pages#index'
-  get 'pages/help'
-
-  resources :users
-end
-Rails.application.routes.draw do
-  get 'topics/new'
-  get 'sessions/new'
-
-  root 'pages#index'
-  get 'pages/help'
-
-  resources :users
-
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-end
-Rails.application.routes.draw do
   get 'sessions/new'
 
   root 'pages#index'
